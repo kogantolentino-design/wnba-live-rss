fdeea52a4e78b3c52c470d459afed795524c31fa
kogantolentino-design <kogantolentino@gmail.com>
2026-08-06 21:20:58 +1200

    Create generate.js

---
+++ generate.js
@@ -0,0 +1,29 @@
+import RSS from "rss";
+import fs from "fs";
+
+const url =
+  "https://site.api.espn.com/apis/site/v2/sports/basketball/wnba/scoreboard";
+
+const data = await fetch(url).then(r => r.json());
+
+const feed = new RSS({
+  title: "WNBA Live Scores",
+  description: "Live WNBA Score Feed",
+  feed_url: "https://YOUR_GITHUB_USERNAME.github.io/wnba-live-rss/feed.xml",
+  site_url: "https://www.wnba.com"
+});
+
+for (const game of data.events) {
+  const comp = game.competitions[0];
+
+  const home = comp.competitors.find(c => c.homeAway === "home");
+  const away = comp.competitors.find(c => c.homeAway === "away");
+
+  feed.item({
+    title: `${away.team.displayName} ${away.score} - ${home.score} ${home.team.displayName}`,
+    description: game.status.type.detail,
+    date: new Date(game.date)
+  });
+}
+
+fs.writeFileSync("feed.xml", feed.xml({ indent: true }));
